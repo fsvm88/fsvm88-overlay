606f7602828c14a286ef8628c763782f3eb98831
Fabio Scaccabarozzi <fsvm88@gmail.com>
2024-11-24 13:15:17 +0100

    Bump some, regenerate manifests, add script to do it

---
+++ scripts/refresh_ebuilds.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+find . -name "*.ebuild" -exec dirname {} + | sort -u | while read -r one_line; do
+    pushd "$one_line" || exit 2
+    for x in *.ebuild; do
+        ebuild "$x" digest
+    done
+    popd || exit 3
+done
